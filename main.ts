cb25fb740cdea2f7b4a7317bb7a2596da85daa38
TPAMarvin <31587596+TPAMarvin@users.noreply.github.com>
2020-07-02 13:03:04 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    strip.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Blue))
 })
+input.onButtonPressed(Button.B, function () {
+    strip.showRainbow(1, 350)
+})
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Red))
+basic.pause(1000)
+strip.clear()
+strip.show()
